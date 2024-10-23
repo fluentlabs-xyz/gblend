0e731f67c290730c35bc90500d10ad8de6223620
protocolwhisper <chrismatai@hotmail.com>
2024-10-23 13:28:16 -0500

    (fix) move web3 to ethers

--- src/deployer/deployer.js
+++ src/deployer/deployer.js
@@ -1,86 +1,87 @@
-const {Web3, ETH_DATA_FORMAT} = require('web3');
-const fs = require('fs');
+const { ethers } = require("ethers");
+const fs = require("fs");
 
-const DEPLOYER_PRIVATE_KEY = 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+// Insert your private key 
+const DEPLOYER_PRIVATE_KEY = '';
 
 const main = async () => {
+    // Check if a WASM binary path is provided
     if (process.argv.length < 3) {
-        console.log(`You must specify path to the WASM binary!`);
-        console.log(`Example: node deploy-contract.js --dev ../bin/greeting.wasm`);
+        console.log(`You must specify the path to the WASM binary!`);
+        console.log(`Example: node deployer.js --dev ../bin/greeting.wasm`);
         process.exit(-1);
     }
+
+    // Read command-line arguments
     let args = process.argv.slice(2);
     const checkFlag = (param) => {
-        let indexOf = args.indexOf(param)
-        if (indexOf < 0) {
-            return false
-        }
-        args.splice(indexOf, 1)
-        return true
+        let index = args.indexOf(param);
+        if (index < 0) return false;
+        args.splice(index, 1);
+        return true;
     };
-    let isLocal = checkFlag('--local')
-    let isDev = checkFlag('--dev')
+    let isLocal = checkFlag('--local');
+    let isDev = checkFlag('--dev');
 
-    let web3Url = '';
-    if (isLocal) {
-        web3Url = 'http://127.0.0.1:8545';
-    } else if (isDev) {
-        web3Url = 'https://rpc.dev.thefluent.xyz/';
-    } else {
+    // Set the provider URL based on the flag
+    let providerUrl = isLocal ? 'http://127.0.0.1:8545' : isDev ? 'https://rpc.dev.thefluent.xyz/' : '';
+    if (!providerUrl) {
         console.log(`You must specify --dev or --local flag!`);
-        console.log(`Example: node deploy-contract.js --dev ./bin/greeting.wasm`);
+        console.log(`Example: node deployer.js --dev ./bin/{yourfile}.wasm`);
         process.exit(-1);
     }
 
+    // Read the WASM binary and convert it to hex
     let [binaryPath] = args;
-    let wasmBinary = fs.readFileSync(binaryPath).toString('hex');
-    const web3 = new Web3(web3Url);
-    let privateKey = process.env.DEPLOYER_PRIVATE_KEY || DEPLOYER_PRIVATE_KEY;
-    let account = web3.eth.accounts.privateKeyToAccount('0x' + privateKey);
+    let wasmBinary;
+    try {
+        wasmBinary = fs.readFileSync(binaryPath).toString('hex');
+    } catch (err) {
+        console.error(`Failed to read the WASM file: ${err.message}`);
+        process.exit(-1);
+    }
 
-    console.log('Signing transaction...');
-    const gasPrice = await web3.eth.getGasPrice(ETH_DATA_FORMAT)
-    const signedTransaction = await web3.eth.accounts.signTransaction({
+    // Set up ethers.js provider and signer
+    const provider = new ethers.JsonRpcProvider(providerUrl);
+    const wallet = new ethers.Wallet(DEPLOYER_PRIVATE_KEY, provider);
+
+    // Create a deployment transaction
+    const tx = {
         data: '0x' + wasmBinary,
-        gasPrice,
-        gas: 10_000_000,
-        from: account.address,
-    }, privateKey)
+        gasLimit: 10_000_000,
+    };
 
-    let contractAddress = '';
-    console.log('Sending transaction...');
-    await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
-       .on('confirmation', confirmation => {
-           contractAddress = confirmation.receipt.contractAddress;
-           console.log(confirmation)
-           if (contractAddress) {
-               console.log(`Contract address is: ${contractAddress}`);
-           }
-       });
+    // Send the transaction
+    console.log('Deploying contract...');
+    try {
+        const txResponse = await wallet.sendTransaction(tx);
+        console.log('Transaction sent:', txResponse.hash);
 
-    const result = await web3.eth.call({
-        to: contractAddress,
-    });
-    function isASCII(str) {
-        return /^[\x00-\x7F]*$/.test(str);
-    }
-    if (isASCII(web3.utils.hexToAscii(result))) {
-        console.log(`Message: "${web3.utils.hexToAscii(result)}"`)
-    } else {
-        console.log(`Message: "${result}"`)
+        // Wait for the transaction to be mined
+        const receipt = await txResponse.wait();
+        console.log('Transaction mined:', receipt);
+
+        // Get the contract address from the receipt
+        const contractAddress = receipt.contractAddress;
+        console.log(`Contract deployed at: ${contractAddress}`);
+    } catch (err) {
+        console.error(`Deployment failed: ${err.message}`);
+        process.exit(-1);
     }
 
-    // const signedTransaction1 = await web3.eth.accounts.signTransaction({
-    //     to: contractAddress,
-    //     gas: 1_000_000,
-    // }, DEPLOYER_PRIVATE_KEY)
-    // const receipt1 = await web3.eth.sendSignedTransaction(signedTransaction1.rawTransaction);
-    // console.log(`Receipt: ${JSON.stringify(receipt1, null, 2)}`)
+    // Fetch the latest block number
+    try {
+        const latestBlockNumber = await provider.getBlockNumber();
+        console.log(`Latest block number: ${latestBlockNumber}`);
+    } catch (err) {
+        console.error(`Failed to fetch the latest block number: ${err.message}`);
+    }
 
-    const latestMinedBlockNumber = await web3.eth.getBlockNumber();
-    console.log(`Latest block number: ${latestMinedBlockNumber}`);
+    process.exit(0);
+};
 
-    process.exit(0)
-}
-
-main().then(console.log).catch(console.error);
+// Execute the main function
+main().catch((err) => {
+    console.error(err);
+    process.exit(-1);
+});
