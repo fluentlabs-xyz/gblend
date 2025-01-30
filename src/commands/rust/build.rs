4d108bd85cca79f8026d97bd78a00f64f57384b4
Dmitry Savonin <nwtsoas@gmail.com>
2025-01-30 22:20:57 +0300

    refactor(build): improve build process and fix verbose output
    - Improved build logic for detecting project metadata and wasm files.
    - Fixed verbose logging behavior and adjusted default verbosity.
    - Enhanced `.wat` file generation from `.wasm`.

--- src/commands/rust/build.rs
+++ src/commands/rust/build.rs
@@ -3,12 +3,11 @@ use crate::error::Error;
 use clap::Args;
 use std::{
     fs,
-    io::{self, BufRead},
     path::{Path, PathBuf},
-    process::{Command, Stdio},
+    process::Command,
+    str::from_utf8,
     time::Instant,
 };
-use std::str::from_utf8;
 
 #[derive(Args)]
 pub struct BuildArgs {
@@ -30,7 +29,7 @@ pub struct BuildArgs {
     wat: bool,
 
     /// Show build logs
-    #[arg(short, long, help = "Show build logs", default_value = "true")]
+    #[arg(short, long, help = "Show build logs", default_value = "false")]
     verbose: bool,
 
     /// Show build logs
@@ -68,12 +67,22 @@ pub(super) fn execute(args: &BuildArgs) -> Result<(), Error> {
     for t in Tool::all(args.wat) {
         t.ensure()?;
     }
-    let result = build_project(&args.path, args.release, args.verbose, args.target_dir.clone())?;
+    let result = build_project(
+        &args.path,
+        args.release,
+        args.verbose,
+        args.target_dir.clone(),
+    )?;
     print_build_result(&result);
     Ok(())
 }
 
-fn build_project(path: &PathBuf, release: bool, verbose: bool, target_dir: Option<String>) -> Result<BuildResult, Error> {
+fn build_project(
+    path: &PathBuf,
+    release: bool,
+    verbose: bool,
+    target_dir: Option<String>,
+) -> Result<BuildResult, Error> {
     println!("🔨 Building Rust project...");
 
     // Validate project structure
@@ -85,7 +94,31 @@ fn build_project(path: &PathBuf, release: bool, verbose: bool, target_dir: Optio
     println!("📦 Running cargo build...");
     run_cargo_build(path, release, verbose, target_dir.clone())?;
 
-    let target_dir = if target_dir.is_none() {
+    let project_name = {
+        let result = Command::new("cargo")
+            .arg("read-manifest")
+            .output()
+            .map_err(|e| {
+                if verbose {
+                    println!("Failed to read manifest: {:?}", e);
+                }
+                Error::Build("Failed to read manifest".to_string())
+            })?;
+        let utf8_string = from_utf8(&result.stdout).map_err(|e| {
+            if verbose {
+                println!("Failed to decode UTF-8 output: {:?}", e);
+            }
+            Error::Build("Failed to get target directory".to_string())
+        })?;
+        let json_value = json::parse(utf8_string).unwrap();
+        json_value["name"].to_string()
+    };
+
+    if verbose {
+        println!(" ~ detected project name: {}", project_name)
+    }
+
+    let (target_dir, library_name) = if target_dir.is_none() {
         let result = Command::new("cargo")
             .arg("metadata")
             .arg("--no-deps")
@@ -102,42 +135,63 @@ fn build_project(path: &PathBuf, release: bool, verbose: bool, target_dir: Optio
             }
             Error::Build("Failed to get target directory".to_string())
         })?;
-        let json_value = json::parse(utf8_string).unwrap();
-        json_value["target_directory"].to_string()
+        let json_value = json::parse(utf8_string).expect("can't parse json with manifest");
+        let package = json_value["packages"]
+            .members()
+            .find(|package| package["name"].to_string() == project_name)
+            .expect("can't find package in the manifest");
+        let library_name = package["targets"].members().find_map(|target| {
+            target["kind"]
+                .members()
+                .find(|lib| lib.to_string() == "cdylib")?;
+            Some(format!("{}.wasm", target["name"].to_string()))
+        });
+        let target_directory = json_value["target_directory"].to_string();
+        (target_directory, library_name)
     } else {
-        target_dir.unwrap()
+        (target_dir.unwrap(), None)
     };
 
+    let library_name =
+        library_name.unwrap_or_else(|| format!("{}.wasm", project_name.replace("-", "_")));
+
     if verbose {
-        println!("Checking target dir: {}", target_dir)
+        println!(" ~ found target dir: {}", target_dir)
     }
 
     // Define the expected output location
     let build_mode = if release { "release" } else { "debug" };
-    let target_dir = path.join(target_dir).join("wasm32-unknown-unknown").join(build_mode);
+    let target_dir = path
+        .join(target_dir)
+        .join("wasm32-unknown-unknown")
+        .join(build_mode);
 
     // Locate the generated .wasm file
-    let wasm_file = target_dir
-        .read_dir()?
-        .filter_map(|entry| entry.ok())
-        .find(|entry| entry.path().extension() == Some("wasm".as_ref()))
-        .ok_or_else(|| Error::Build("No .wasm file found in target directory".to_string()))?;
+    let wasm_file = PathBuf::from(target_dir.to_str().unwrap()).join(library_name);
+
+    if verbose {
+        println!(
+            " ~ detected wasm binary path: {}",
+            wasm_file.to_str().unwrap_or_default()
+        )
+    }
 
     // Copy the .wasm file to `lib.wasm`
     let final_wasm_path = path.join("lib.wasm");
-    fs::copy(wasm_file.path(), &final_wasm_path)?;
+    fs::copy(wasm_file, &final_wasm_path)?;
 
-    // Optionally convert to .wat format using wasm2wat
-    if Command::new("wasm2wat")
-        .arg(&final_wasm_path)
-        .output()
-        .is_ok()
-        && verbose
-    {
-        println!(
-            "💡 Generated .wat file from .wasm at {:?}/lib.wat",
-            path.to_str()
-        );
+    // Optionally convert to .wat format
+    let wasm2wat = Command::new("wasm2wat").arg(&final_wasm_path).output();
+    if wasm2wat.is_ok() {
+        let wasm2wat = wasm2wat.unwrap();
+        let final_wast_path = path.join("lib.wat");
+        fs::write(final_wast_path, from_utf8(&wasm2wat.stdout).unwrap())?;
+        if verbose {
+            println!(
+                "💡 Generated .wat file from .wasm at {:?}/lib.wat",
+                path.to_str()
+            );
+        }
     }
 
     // Gather metadata
@@ -155,7 +209,12 @@ fn build_project(path: &PathBuf, release: bool, verbose: bool, target_dir: Optio
     })
 }
 
-fn run_cargo_build(path: &PathBuf, release: bool, verbose: bool, target_dir: Option<String>) -> Result<(), Error> {
+fn run_cargo_build(
+    path: &PathBuf,
+    release: bool,
+    verbose: bool,
+    target_dir: Option<String>,
+) -> Result<(), Error> {
     let mut build_args = vec![
         "build".to_string(),
         "--target".to_string(),
@@ -171,37 +230,39 @@ fn run_cargo_build(path: &PathBuf, release: bool, verbose: bool, target_dir: Opt
     }
 
     if verbose {
-        println!("Running command: {}", build_args.join(" "));
+        println!("~ running command: {}", build_args.join(" "));
     }
 
-    let mut cmd = Command::new("cargo")
-        .args(&build_args)
+    let mut cmd = Command::new("cargo");
+    cmd.args(&build_args)
         .env(
             "RUSTFLAGS",
             "-C link-arg=-zstack-size=262144 -C target-feature=+bulk-memory",
         )
-        .current_dir(path)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
+        .current_dir(path);
+    // if verbose {
+    //     cmd.stdout(Stdio::piped()).stderr(Stdio::piped());
+    // }
+    let cmd = cmd
         .spawn()
         .map_err(|e| Error::Build(format!("Failed to start build process: {}", e)))?;
 
     // Stream output line by line in verbose mode
-    if verbose {
-        if let Some(stdout) = cmd.stdout.take() {
-            let stdout_reader = io::BufReader::new(stdout);
-            for line in stdout_reader.lines().map_while(Result::ok) {
-                println!("{}", line);
-            }
-        }
-
-        if let Some(stderr) = cmd.stderr.take() {
-            let stderr_reader = io::BufReader::new(stderr);
-            for line in stderr_reader.lines().map_while(Result::ok) {
-                eprintln!("{}", line);
-            }
-        }
-    }
+    // if verbose {
+    //     if let Some(stdout) = cmd.stdout.take() {
+    //         let stdout_reader = io::BufReader::new(stdout);
+    //         for line in stdout_reader.lines().map_while(Result::ok) {
+    //             println!("{}", line);
+    //         }
+    //     }
+    //
+    //     if let Some(stderr) = cmd.stderr.take() {
+    //         let stderr_reader = io::BufReader::new(stderr);
+    //         for line in stderr_reader.lines().map_while(Result::ok) {
+    //             eprintln!("{}", line);
+    //         }
+    //     }
+    // }
 
     // Wait for the command to finish and check if it was successful
     let output = cmd
@@ -280,14 +341,14 @@ fn print_build_result(result: &BuildResult) {
     println!("📊 Size: {} bytes", result.size);
 
     if let Some(metadata) = &result.metadata {
-        println!("⚙️  Compiler: {}", metadata.compiler_version);
+        println!("⚙️ Compiler: {}", metadata.compiler_version);
         println!("🎯 Target: {}", metadata.target);
         println!("⚡ Optimization: {}", metadata.optimization_level);
-        println!("⏱️  Build time: {:?}", metadata.build_time);
+        println!("⏱️ Build time: {:?}", metadata.build_time);
     }
 
     if let Some(warnings) = &result.warnings {
-        println!("\n⚠️  Warnings:");
+        println!("\n⚠️ Warnings:");
         for warning in warnings {
             println!("  {}", warning);
         }
