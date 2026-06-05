b5ce6c7dd4753d133dad46cbf858d7168f6b03f4
hedwig0x <hedwig0x@users.noreply.github.com>
2026-06-05 05:38:12 +0000

    fix(ci): use active depot runner labels

--- .github/scripts/matrices.py
+++ .github/scripts/matrices.py
@@ -68,7 +68,7 @@ class Expanded:
 profile = os.environ.get("PROFILE")
 is_pr = os.environ.get("EVENT_NAME") == "pull_request"
 t_linux_x86 = Target(
-    "depot-ubuntu-22.04-16", "x86_64-unknown-linux-gnu", "linux-amd64"
+    "depot-ubuntu-latest-16", "x86_64-unknown-linux-gnu", "linux-amd64"
 )
 t_linux_arm = Target(
     "depot-ubuntu-latest-arm-16", "aarch64-unknown-linux-gnu", "linux-aarch64"
